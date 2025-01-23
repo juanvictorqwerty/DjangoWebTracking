1f0eb8005bd763647cb65b06aad06b25be80b788
Juan <juanvictorqwerty@gmail.com>
2025-01-23 02:24:39 +0100

    just a fix (forgot the accuracy)

--- App/models.py
+++ App/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class BusCoordinates(models.Model):
     latitude = models.FloatField(default=0.0, null=True, blank=True,)
     longitude = models.FloatField(default=0.0, null=True, blank=True)
+    accuracy = models.FloatField(default=0.0, null=True, blank=True)
 
     def __str__(self):
         return f"{self.latitude}, {self.longitude}"

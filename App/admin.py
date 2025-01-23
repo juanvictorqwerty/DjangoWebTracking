31e1dcb6d43ca002b1c74eb002ae8ccb2e5d25f9
Juan <juanvictorqwerty@gmail.com>
2025-01-23 02:21:16 +0100

    added the table bus coordinates

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import BusCoordinates
 # Register your models here.
+
+admin.site.register(BusCoordinates)
--- App/models.py
+++ App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BusCoordinates(models.Model):
+    latitude = models.FloatField(default=0.0, null=True, blank=True,)
+    longitude = models.FloatField(default=0.0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.latitude}, {self.longitude}"
